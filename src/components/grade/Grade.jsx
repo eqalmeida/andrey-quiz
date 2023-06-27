ab3dc1ccf11e6edb20269c0ba6c28724d9e12a37
Eduardo Almeida <eqalmeida@gmail.com>
2023-06-27 13:58:05 -0300

    Add margin in grade card

--- src/components/grade/Grade.jsx
+++ src/components/grade/Grade.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Grade({ points, total, resetClick }) {
   const percent = (points * 100) / total;
   return (
-    <div className="card">
+    <div className="card mt-4">
       <div className="card-body">
         <h5 className="card-title">Pontuação</h5>
         <div>
